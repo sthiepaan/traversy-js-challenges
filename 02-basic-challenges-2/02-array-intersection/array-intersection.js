c2b407ef3a459fc8086137df89de869e7f336bd4
sthiepaan <info@damianszczypka.com>
2026-04-27 23:05:54 +0200

    DSA-0202 Implement 'array-intersection' Challenge from 'basic-challenges-2' Module

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,14 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+	const uniqueNumbers = new Set(arr1);
+	const commonNumbers = [];
+
+	for (const number of arr2) {
+		if (uniqueNumbers.has(number)) {
+			commonNumbers.push(number);
+		}
+	}
+
+	return commonNumbers;
+}
 
 module.exports = arrayIntersection;
