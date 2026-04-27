a93b9cac1fa3618018d62b350210485fa3f7c675
sthiepaan <info@damianszczypka.com>
2026-04-27 22:40:36 +0200

    DSA-0201 Implement 'fizzbuzz-array' Challenge from 'basic-challenges-2' Module

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,23 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(length) {
+	return Array.from({ length }).map((_, index) => {
+		const num = index + 1;
+		const fizz = num % 3 === 0;
+		const buzz = num % 5 === 0;
+
+		if (fizz && buzz) {
+			return 'FizzBuzz';
+		}
+
+		if (fizz) {
+			return 'Fizz';
+		}
+
+		if (buzz) {
+			return 'Buzz';
+		}
+
+		return num;
+	});
+}
 
 module.exports = fizzBuzzArray;
