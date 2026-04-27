d7216ba4325a30e0448e56968a1010ed2fdda915
sthiepaan <info@damianszczypka.com>
2026-04-27 23:47:56 +0200

    DSA-0204 Implement 'find-missing-number' Challenge from 'basic-challenges-2' Module

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,14 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+	const sortedNumbers = arr.toSorted((a, b) => a - b);
+
+	for (let index = 0; index < sortedNumbers.length; index++) {
+		const currNumber = sortedNumbers[index];
+		const nextNumber = sortedNumbers[index + 1] - 1;
+
+		if (currNumber !== nextNumber) {
+			return nextNumber;
+		}
+	}
+}
 
 module.exports = findMissingNumber;
