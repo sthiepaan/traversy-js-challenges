e2d699755c91d4eb118b4698781806aa3b57d852
sthiepaan <info@damianszczypka.com>
2026-04-29 00:10:30 +0200

    DSA-0209 Implement 'format-phone-number' Challenge from 'basic-challenges-2' Module

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,5 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(numbers) {
+	return numbers.join('').replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
+}
 
 module.exports = formatPhoneNumber;
