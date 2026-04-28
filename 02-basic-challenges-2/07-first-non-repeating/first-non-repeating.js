5e5986da05ab3316ee2a8d30653366eb24d3971f
sthiepaan <info@damianszczypka.com>
2026-04-28 23:40:39 +0200

    DSA-0207 Implement 'first-non-repeating' Challenge from 'basic-challenges-2' Module

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,17 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+	const chars = new Map();
+
+	for (const char of str) {
+		chars.set(char, chars.has(char) ? chars.get(char) + 1 : 1);
+	}
+
+	for (const [char, count] of chars) {
+		if (count === 1) {
+			return char;
+		}
+	}
+
+	return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
