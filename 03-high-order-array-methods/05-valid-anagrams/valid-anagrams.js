119a6c2bde3f0cf497b5d9d14b0ade3e010f25e0
sthiepaan <info@damianszczypka.com>
2026-04-30 23:58:14 +0200

    DSA-0305 Implement 'valid-anagrams' Challenge from 'high-order-array-methods' Module

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,5 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+	return str1.split('').toSorted().join('') === str2.split('').toSorted().join('');
+}
 
 module.exports = validAnagrams;
