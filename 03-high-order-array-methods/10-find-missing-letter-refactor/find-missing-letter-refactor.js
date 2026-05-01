c6fee89e163dc7f937b6dc1d99a9598094e3aefa
sthiepaan <info@damianszczypka.com>
2026-05-01 22:44:00 +0200

    DSA-0310 Implement 'find-missing-letter-refactor' Challenge from 'high-order-array-methods' Module

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,9 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+	const charCode = arr
+		.map((char) => char.charCodeAt(0))
+		.find((charCode, index, mappedArr) => charCode - mappedArr[index - 1] > 1);
+
+	return String.fromCharCode(charCode - 1);
+}
 
 module.exports = findMissingLetter;
