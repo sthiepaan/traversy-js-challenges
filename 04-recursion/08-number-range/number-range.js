4c489fc4b50a9b814887de1e1b163f6b38f6f838
sthiepaan <info@damianszczypka.com>
2026-05-02 23:39:44 +0200

    DSA-0408 Implement 'number-range' Challenge from 'recursion' Module

--- 04-recursion/08-number-range/number-range.js
+++ 04-recursion/08-number-range/number-range.js
@@ -1,3 +1,13 @@
-function numberRange() {}
+function numberRange(startNum, endNum) {
+	if (endNum === startNum) {
+		return [endNum];
+	}
+
+	const arr = numberRange(startNum, endNum - 1);
+
+	arr.push(endNum);
+
+	return arr;
+}
 
 module.exports = numberRange;
