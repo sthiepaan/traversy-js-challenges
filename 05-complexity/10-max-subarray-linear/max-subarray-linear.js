afc8e3b4e9443b59d06ffc2856532bfc1a6635a8
sthiepaan <info@damianszczypka.com>
2026-05-06 22:48:44 +0200

    DSA-0510 Implement 'max-subarray-linear' Challenge from 'complexity' Module

--- 05-complexity/10-max-subarray-linear/max-subarray-linear.js
+++ 05-complexity/10-max-subarray-linear/max-subarray-linear.js
@@ -1,3 +1,18 @@
-function maxSubarraySum() {}
+function maxSubarraySum(arr, k) {
+	let winSum = 0;
+
+	for (let i = 0; i < k; i++) {
+		winSum += arr[i];
+	}
+
+	let maxSum = winSum;
+
+	for (let i = k; i < arr.length; i++) {
+		winSum += arr[i] - arr[i - k];
+		maxSum = Math.max(maxSum, winSum);
+	}
+
+	return maxSum;
+}
 
 module.exports = maxSubarraySum;
