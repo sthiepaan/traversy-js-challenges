b1d6125ab8803b148c748a679ac059352198e97f
sthiepaan <info@damianszczypka.com>
2026-05-09 22:42:05 +0200

    DSA-0608 Implement 'symmetric-difference' Challenge from 'hash-tables-maps-sets' Module

--- 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
+++ 06-hash-tables-maps-sets/08-symmetric-difference/symmetric-difference.js
@@ -1,3 +1,16 @@
-function symmetricDifference() {}
+function symmetricDifference(arr1, arr2) {
+	const set1 = new Set(arr1);
+	const set2 = new Set(arr2);
+
+	for (const num of set2) {
+		if (set1.has(num)) {
+			set1.delete(num);
+		} else {
+			set1.add(num);
+		}
+	}
+
+	return Array.from(set1);
+}
 
 module.exports = symmetricDifference;
