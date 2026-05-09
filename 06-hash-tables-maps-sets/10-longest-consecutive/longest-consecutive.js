ad7db2af846f0ce8d9ffdfd0d6153e9fb3fb40fc
sthiepaan <info@damianszczypka.com>
2026-05-09 23:56:02 +0200

    DSA-0610 Implement 'longest-consecutive' Challenge from 'hash-tables-maps-sets' Module

--- 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
+++ 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
@@ -1,3 +1,23 @@
-function longestConsecutiveSequence() {}
+function longestConsecutiveSequence(nums) {
+	let currSequence = 0;
+	let maxSequence = 0;
+
+	for (let i = 0; i < nums.sort((a, b) => a - b).length; i++) {
+		const currNum = nums[i];
+		const prevNum = nums[i - 1];
+
+		if (prevNum && currNum - prevNum !== 1) {
+			currSequence = 1;
+		} else {
+			currSequence++;
+		}
+
+		if (currSequence > maxSequence) {
+			maxSequence = currSequence;
+		}
+	}
+
+	return maxSequence;
+}
 
 module.exports = longestConsecutiveSequence;
