8b8050a1e3aa49d6c696072337db850c015b2ea2
sthiepaan <info@damianszczypka.com>
2026-05-09 23:26:39 +0200

    DSA-0609 Implement 'two-sum' Challenge from 'hash-tables-maps-sets' Module

--- 06-hash-tables-maps-sets/09-two-sum/two-sum.js
+++ 06-hash-tables-maps-sets/09-two-sum/two-sum.js
@@ -1,3 +1,18 @@
-function twoSum() {}
+function twoSum(nums, target) {
+	const map = new Map();
+
+	for (let i = 0; i < nums.length; i++) {
+		const num = nums[i];
+		const num2Index = map.get(target - num);
+
+		if (typeof num2Index !== 'undefined') {
+			return [num2Index, i];
+		}
+
+		map.set(num, i);
+	}
+
+	return [];
+}
 
 module.exports = twoSum;
