d5b7174bad295446c8803b63c5ffbc877538e3cd
sthiepaan <info@damianszczypka.com>
2026-05-10 22:05:29 +0200

    DSA-0611 Implement 'custom-hash-table' Challenge from 'hash-tables-maps-sets' Module

--- 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table.js
+++ 06-hash-tables-maps-sets/11-custom-hash-table/custom-hash-table.js
@@ -1,3 +1,67 @@
-class HashTable {}
+class HashTable {
+	constructor(limit = 10) {
+		this.storage = [];
+		this.limit = limit;
+	}
+
+	#hash(key, max) {
+		let hash = 0;
+
+		for (let i = 0; i < key.length; i++) {
+			hash += key.charCodeAt(i);
+		}
+
+		return hash % max;
+	}
+
+	set(key, val) {
+		const hash = this.#hash(key, this.limit);
+
+		if (this.storage[hash]) {
+			let inserted = false;
+
+			for (let i = 0; i < this.storage[hash].length; i++) {
+				if (this.storage[hash][i][0] !== key) continue;
+
+				this.storage[hash][i][1] = val;
+				inserted = true;
+			}
+
+			if (inserted) return;
+
+			this.storage[hash].push([key, val]);
+		} else {
+			this.storage[hash] = [[key, val]];
+		}
+	}
+
+	get(key) {
+		const hash = this.#hash(key, this.limit);
+
+		if (!this.storage[hash]) return;
+
+		for (let i = 0; i < this.storage[hash].length; i++) {
+			if (this.storage[hash][i][0] !== key) continue;
+
+			return this.storage[hash][i][1];
+		}
+	}
+
+	remove(key) {
+		const hash = this.#hash(key, this.limit);
+
+		if (!this.storage[hash]) return;
+
+		if (this.storage[hash].length === 1 && this.storage[hash][0][0] === key) {
+			delete this.storage[hash];
+		} else {
+			for (let i = 0; i < this.storage[hash].length; i++) {
+				if (this.storage[hash][i][0] !== key) continue;
+
+				delete this.storage[hash][i];
+			}
+		}
+	}
+}
 
 module.exports = HashTable;
