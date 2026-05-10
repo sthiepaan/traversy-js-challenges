3bede738feae405fb5793c18530759c97eda5c89
sthiepaan <info@damianszczypka.com>
2026-05-10 22:38:12 +0200

    DSA-0612 Implement 'word-instance-counter' Challenge from 'hash-tables-maps-sets' Module

--- 06-hash-tables-maps-sets/12-word-instance-counter/word-instance-counter.js
+++ 06-hash-tables-maps-sets/12-word-instance-counter/word-instance-counter.js
@@ -1,5 +1,15 @@
 const HashTable = require('./HashTable');
 
-function wordInstanceCounter() {}
+function wordInstanceCounter(str, word) {
+	const hashTable = new HashTable();
+
+	for (const currWord of str.toLowerCase().split(/\W+/)) {
+		if (!currWord) continue;
+
+		hashTable.set(currWord, hashTable.has(currWord) ? hashTable.get(currWord) + 1 : 1);
+	}
+
+	return hashTable.get(word);
+}
 
 module.exports = wordInstanceCounter;
