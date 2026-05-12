7466090140066a30792ef3069cbb778f72616f72
sthiepaan <info@damianszczypka.com>
2026-05-12 08:01:08 +0200

    DSA-0703 Implement 'reverse-string-stack' Challenge from 'stacks-queues-linked-lists' Module

--- 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
+++ 07-stacks-queues-linked-lists/03-reverse-string-stack/reverse-string-stack.js
@@ -1,5 +1,18 @@
 const Stack = require('./stack');
 
-function reverseStringStack() {}
+function reverseStringStack(str) {
+	const stack = new Stack();
+	let reversedStr = '';
+
+	for (const char of str) {
+		stack.push(char);
+	}
+
+	while (!stack.isEmpty()) {
+		reversedStr += stack.pop();
+	}
+
+	return reversedStr;
+}
 
 module.exports = reverseStringStack;
