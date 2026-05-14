9689982ab4bf4e0ba6d7b522b87574ac7ad101eb
sthiepaan <info@damianszczypka.com>
2026-05-14 22:06:01 +0200

    DSA-0706 Implement 'queue-implementation' Challenge from 'stacks-queues-linked-lists' Module

--- 07-stacks-queues-linked-lists/06-queue-implementation/queue.js
+++ 07-stacks-queues-linked-lists/06-queue-implementation/queue.js
@@ -1,3 +1,41 @@
-class Queue {}
+class Queue {
+	constructor() {
+		this.maxSize = 100;
+		this.queue = [];
+		this.head = 0;
+		this.tail = 0;
+	}
+
+	getLength() {
+		return this.tail - this.head;
+	}
+
+	isFull() {
+		return this.getLength() === this.maxSize;
+	}
+
+	isEmpty() {
+		return this.getLength() === 0;
+	}
+
+	enqueue(value) {
+		if (this.isFull()) return;
+
+		this.queue[this.tail] = value;
+		this.tail += 1;
+	}
+
+	dequeue() {
+		const value = this.queue[this.head];
+
+		this.head += 1;
+
+		return value;
+	}
+
+	peek() {
+		return this.queue[this.head];
+	}
+}
 
 module.exports = Queue;
