134d3c158d01efda42122d99c25da74211acd8b2
sthiepaan <info@damianszczypka.com>
2026-05-14 22:43:43 +0200

    DSA-0708 Implement 'palindrome-queue-stack' Challenge from 'stacks-queues-linked-lists' Module

--- 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
+++ 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack.js
@@ -1,6 +1,22 @@
 const Queue = require('./queue');
 const Stack = require('./stack');
 
-function isPalindromeQueueStack() {}
+function isPalindromeQueueStack(str) {
+	const queue = new Queue();
+	const stack = new Stack();
+
+	for (const char of str.toLowerCase().replace(/\W+/g, '')) {
+		queue.enqueue(char);
+		stack.push(char);
+	}
+
+	while (!queue.isEmpty() && !stack.isEmpty()) {
+		if (queue.dequeue() !== stack.pop()) {
+			return false;
+		}
+	}
+
+	return true;
+}
 
 module.exports = isPalindromeQueueStack;
