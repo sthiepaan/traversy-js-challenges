1de2f52bf432dfb550cf28ff3abd209a55a16e64
sthiepaan <info@damianszczypka.com>
2026-05-18 16:36:20 +0200

    DSA-0804 Implement 'depth-first-traversal-recursive' Challenge from 'binary-trees-graphs' Module

--- 08-binary-trees-graphs/04-depth-first-traversal-recursive/depth-first-traversal-recursive.js
+++ 08-binary-trees-graphs/04-depth-first-traversal-recursive/depth-first-traversal-recursive.js
@@ -1,8 +1,28 @@
-class Node {}
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
 
-function recDepthFirstTraversal() {}
+function recDepthFirstTraversal(root) {
+	const values = [];
+
+	function traverse(node) {
+		if (!node) return;
+
+		values.push(node.value);
+		traverse(node.left);
+		traverse(node.right);
+	}
+
+	traverse(root);
+
+	return values;
+}
 
 module.exports = {
-  Node,
-  recDepthFirstTraversal,
+	Node,
+	recDepthFirstTraversal,
 };
