b8ac8f3246e4890912fc5fd4909f65195f1dd915
sthiepaan <info@damianszczypka.com>
2026-05-18 07:47:59 +0200

    DSA-0803 Implement 'depth-first-traversal' Challenge from 'binary-trees-graphs' Module

--- 08-binary-trees-graphs/03-depth-first-traversal/depth-first-traversal.js
+++ 08-binary-trees-graphs/03-depth-first-traversal/depth-first-traversal.js
@@ -1,10 +1,41 @@
 const Stack = require('./stack');
 
-class Node {}
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
 
-function depthFirstTraversal() {}
+function depthFirstTraversal(root) {
+	if (!root) {
+		return [];
+	}
+
+	const stack = new Stack();
+	const values = [];
+
+	stack.push(root);
+
+	while (!stack.isEmpty()) {
+		let node = stack.pop();
+
+		values.push(node.value);
+
+		if (node.right) {
+			stack.push(node.right);
+		}
+
+		if (node.left) {
+			stack.push(node.left);
+		}
+	}
+
+	return values;
+}
 
 module.exports = {
-  Node,
-  depthFirstTraversal,
+	Node,
+	depthFirstTraversal,
 };
