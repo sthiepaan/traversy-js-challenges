22562215e8baf897d6b2dbc431707f965a9025b1
sthiepaan <info@damianszczypka.com>
2026-05-22 21:43:32 +0200

    DSA-0805 Implement 'breadth-first-traversal' Challenge from 'binary-trees-graphs' Module

--- 08-binary-trees-graphs/05-breadth-first-traversal/breadth-first-traversal.js
+++ 08-binary-trees-graphs/05-breadth-first-traversal/breadth-first-traversal.js
@@ -1,10 +1,41 @@
 const Queue = require('./queue');
 
-class Node {}
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
 
-function breadthFirstTraversal() {}
+function breadthFirstTraversal(root) {
+	if (!root) {
+		return [];
+	}
+
+	const queue = new Queue();
+	const values = [];
+
+	queue.enqueue(root);
+
+	while (!queue.isEmpty()) {
+		const curr = queue.dequeue();
+
+		values.push(curr.value);
+
+		if (curr.left) {
+			queue.enqueue(curr.left);
+		}
+
+		if (curr.right) {
+			queue.enqueue(curr.right);
+		}
+	}
+
+	return values;
+}
 
 module.exports = {
-  Node,
-  breadthFirstTraversal,
+	Node,
+	breadthFirstTraversal,
 };
