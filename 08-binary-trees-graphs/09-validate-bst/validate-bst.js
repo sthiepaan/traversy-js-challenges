0372d8c2661fb608ba67e458a32353e34e95eb78
sthiepaan <info@damianszczypka.com>
2026-05-25 15:17:42 +0200

    DSA-0809 Implement 'validate-bst' Challenge from 'binary-trees-graphs' Module

--- 08-binary-trees-graphs/09-validate-bst/validate-bst.js
+++ 08-binary-trees-graphs/09-validate-bst/validate-bst.js
@@ -1,11 +1,24 @@
 class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
 }
 
-function isValidBST() {}
+function isValidBST(root, min = null, max = null) {
+	if (!root) {
+		return true;
+	}
+
+	if ((min !== null && root.value <= min) || (max !== null && root.value >= max)) {
+		return false;
+	}
+
+	const isValidBSTLeft = isValidBST(root.left, min, root.value);
+	const isValidBSTRight = isValidBST(root.right, root.value, max);
+
+	return isValidBSTLeft && isValidBSTRight;
+}
 
 module.exports = { Node, isValidBST };
