e17dfd360112155541e9fb349c30ddf12ef258e8
sthiepaan <info@damianszczypka.com>
2026-05-27 06:18:18 +0200

    DSA-0814 Implement 'graph-depth-first-traversal' Challenge from 'binary-trees-graphs' Module

--- 08-binary-trees-graphs/14-graph-depth-first-traversal/graph-depth-first.js
+++ 08-binary-trees-graphs/14-graph-depth-first-traversal/graph-depth-first.js
@@ -1,5 +1,33 @@
 const Stack = require('./stack');
 
-function depthFirstTraversal() {}
+function depthFirstTraversal(graph, startingVertex) {
+	if (!graph.adjacencyList[startingVertex]) {
+		return [];
+	}
+
+	const stack = new Stack();
+	const visited = {};
+	const values = [];
+
+	stack.push(startingVertex);
+
+	visited[startingVertex] = true;
+
+	while (!stack.isEmpty()) {
+		const curr = stack.pop();
+
+		values.push(curr);
+
+		for (const vertex of graph.adjacencyList[curr]) {
+			if (!visited[vertex]) {
+				stack.push(vertex);
+
+				visited[vertex] = true;
+			}
+		}
+	}
+
+	return values;
+}
 
 module.exports = depthFirstTraversal;
