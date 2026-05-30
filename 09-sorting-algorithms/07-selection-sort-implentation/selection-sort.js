7e1dadad6513b7fe5adb093f38de521e467c79b0
sthiepaan <info@damianszczypka.com>
2026-05-30 21:59:00 +0200

    DSA-0907 Implement 'selection-sort-implementation' Challenge from 'sorting-algorithms' Module

--- 09-sorting-algorithms/07-selection-sort-implentation/selection-sort.js
+++ 09-sorting-algorithms/07-selection-sort-implentation/selection-sort.js
@@ -1,3 +1,26 @@
-function selectionSort() {}
+function selectionSort(array) {
+	let index = 0;
+
+	while (index < array.length - 1) {
+		let swapIndex = index;
+
+		for (let nextIndex = index + 1; nextIndex < array.length; nextIndex++) {
+			if (array[nextIndex] < array[swapIndex]) {
+				swapIndex = nextIndex;
+			}
+		}
+
+		if (index !== swapIndex) {
+			const tmp = array[index];
+
+			array[index] = array[swapIndex];
+			array[swapIndex] = tmp;
+		}
+
+		index += 1;
+	}
+
+	return array;
+}
 
 module.exports = selectionSort;
