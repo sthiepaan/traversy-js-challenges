5439fd996b0ad6568a09b3d80e5ed25a313a2fe7
sthiepaan <info@damianszczypka.com>
2026-05-31 19:41:43 +0200

    DSA-0911 Implement 'quick-sort-implementation' Challenge from 'sorting-algorithms' Module

--- 09-sorting-algorithms/11-quick-sort-implementation/quick-sort.js
+++ 09-sorting-algorithms/11-quick-sort-implementation/quick-sort.js
@@ -1,5 +1,21 @@
-function quickSort() {
+function quickSort(array) {
+	if (array.length <= 1) {
+		return array;
+	}
 
+	const pivot = array[0];
+	const arrayLeft = [];
+	const arrayRight = [];
+
+	for (let index = 1; index < array.length; index++) {
+		if (array[index] < pivot) {
+			arrayLeft.push(array[index]);
+		} else {
+			arrayRight.push(array[index]);
+		}
+	}
+
+	return [...quickSort(arrayLeft), pivot, ...quickSort(arrayRight)];
 }
 
 module.exports = quickSort;
