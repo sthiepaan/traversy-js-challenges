4447cc7f5fd7e30aa204b7c29516f91a13216090
sthiepaan <info@damianszczypka.com>
2026-05-31 15:30:55 +0200

    DSA-0909 Implement 'merge-sort-implementation' Challenge from 'sorting-algorithms' Module

--- 09-sorting-algorithms/09-merge-sort-implementation/merge-sort.js
+++ 09-sorting-algorithms/09-merge-sort-implementation/merge-sort.js
@@ -1,3 +1,33 @@
-function mergeSort() {}
+function mergeSort(array) {
+	if (array.length <= 1) {
+		return array;
+	}
+
+	const indexMiddle = Math.floor(array.length / 2);
+	const arrayLeft = mergeSort(array.slice(0, indexMiddle));
+	const arrayRight = mergeSort(array.slice(indexMiddle));
+
+	return merge(arrayLeft, arrayRight);
+}
+
+function merge(arrayLeft, arrayRight) {
+	const array = [];
+	let indexLeft = 0;
+	let indexRight = 0;
+
+	while (indexLeft < arrayLeft.length && indexRight < arrayRight.length) {
+		if (arrayLeft[indexLeft] <= arrayRight[indexRight]) {
+			array.push(arrayLeft[indexLeft]);
+
+			indexLeft += 1;
+		} else {
+			array.push(arrayRight[indexRight]);
+
+			indexRight += 1;
+		}
+	}
+
+	return [...array, ...arrayLeft.slice(indexLeft), ...arrayRight.slice(indexRight)];
+}
 
 module.exports = mergeSort;
